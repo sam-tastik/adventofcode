df212252371fc4ac831bec6b29689cdd9d5873cd
sam-tastik <152223349+sam-tastik@users.noreply.github.com>
2023-11-30 20:11:59 +0000

    parte2 del giorno 1 del 2022

--- 2022/1/elfo.py
+++ 2022/1/elfo.py
@@ -1,21 +1,54 @@
+import sys
+
 print("Ciao a tutti")
+print("SYS:ARG -->  " + sys.argv[1])
+#nome_diario = "diario_elfi.txt"
+nome_diario =  sys.argv[1]
 
-nome_diario = "diario_elfi.txt"
-
-f = open(nome_diario, 'r')
+f = open(nome_diario, 'r', encoding='utf-8')
 contenuto = f.readlines()
+
+# cerco le calorie dello zaino con più calorie
+calorie_di_un_elfo = 0
+numero_massimo_calorie = 0
+i_tre_con_piu_calorie = [0, 0, 0]
+for linea in contenuto:
+    if linea == '\n':
+        # E' finito uno zaino
+        if calorie_di_un_elfo > numero_massimo_calorie:
+            # Questo zaino è quello con più calorie
+            numero_massimo_calorie = calorie_di_un_elfo
+        # resetto per prepararmi a contare le calorie del 
+        # prossimo zaino
+        calorie_di_un_elfo = 0
+    else:
+        # siamo dentro lo zaino, continuiamo a sommare le calorie
+        calorie_di_un_elfo += int(linea)
+# Arrivato qui so il mumero di calorie nello zaino che ne ha di più
+
+# Cerco il numero di calorie del secondo zaino con più calorie
+calorie_di_un_elfo = 0
+numero_massimo_calorie2 = 0
+for linea in contenuto:
+    if linea == '\n':
+        if calorie_di_un_elfo > numero_massimo_calorie2 and calorie_di_un_elfo < numero_massimo_calorie:
+            numero_massimo_calorie2 = calorie_di_un_elfo
+        calorie_di_un_elfo = 0
+    else:
+        calorie_di_un_elfo += int(linea)        
 
 numero_elfi = 1
 calorie_di_un_elfo = 0
-numero_massimo_calorie = 0
+numero_massimo_calorie3 = 0
+i_tre_con_piu_calorie = [0, 0, 0]
 for linea in contenuto:
     if linea == '\n':
-        numero_elfi = numero_elfi + 1
-        print("Questo elfo aveva " + str(calorie_di_un_elfo) + " calorie.")
-        if calorie_di_un_elfo > numero_massimo_calorie:
-            numero_massimo_calorie = calorie_di_un_elfo
+        if calorie_di_un_elfo > numero_massimo_calorie3 and calorie_di_un_elfo < numero_massimo_calorie2:
+            numero_massimo_calorie3 = calorie_di_un_elfo
         calorie_di_un_elfo = 0
     else:
-        calorie_di_un_elfo = calorie_di_un_elfo + int(linea)
+        calorie_di_un_elfo += int(linea)
 
 print(numero_massimo_calorie)
+print(numero_massimo_calorie2)
+print(numero_massimo_calorie3)
