c908633cba21631e2015de5a7a5cbcdce76f51f6
sam-tastik <152223349+sam-tastik@users.noreply.github.com>
2023-12-11 20:21:45 +0000

    2023 fine giorno 1

--- 2023/1/elfo.py
+++ 2023/1/elfo.py
@@ -30,14 +30,48 @@ def estrai_numero(testo, numero_in_lettere, numero_in_cifre):
 
 def trasforma_riga(testo_originale):
     print(f"testo_originale:{testo_originale}")
-    ret = []
-    for posizione_da_cui_partire in range(len(testo_originale)):
-        print(f"posizione_da_cui_partire:{posizione_da_cui_partire}")
-        if testo_originale[posizione_da_cui_partire].isdigit():
-            print(f"La {posizione_da_cui_partire}-sima lettera e' {testo_originale[posizione_da_cui_partire]} ed e' un NUMERO!!!")
-            ret.append(testo_originale[posizione_da_cui_partire])
-        
-    return ''.join(ret)
+    
+    # prepara una variabile dove salvare il risultato
+    tradotto = ''
+
+    for posizione, lettera in enumerate(testo_originale):
+        if lettera.isdigit():
+            #print(f"La lettera '{lettera}' ed e' un NUMERO!!!")
+            tradotto += lettera
+        elif testo_originale[posizione:posizione+len('one')] == 'one':
+            #print("Qui 'one'")
+            tradotto +=  '1'
+        elif testo_originale[posizione:posizione+len('two')] == 'two':
+            #print("Qui 'two'")
+            tradotto +=  '2'
+        elif testo_originale[posizione:posizione+len('three')] == 'three':
+            #print("Qui 'three'")
+            tradotto +=  '3'
+        elif testo_originale[posizione:posizione+len('four')] == 'four':
+            #print("Qui 'four'")
+            tradotto +=  '4'
+        elif testo_originale[posizione:posizione+len('five')] == 'five':
+            #print("Qui 'five'")
+            tradotto +=  '5'
+        elif testo_originale[posizione:posizione+len('six')] == 'six':
+            #print("Qui 'six'")
+            tradotto +=  '6'
+        elif testo_originale[posizione:posizione+len('seven')] == 'seven':
+            #print("Qui 'seven'")
+            tradotto +=  '7'
+        elif testo_originale[posizione:posizione+len('eight')] == 'eight':
+            #print("Qui 'eight'")
+            tradotto +=  '8'
+        elif testo_originale[posizione:posizione+len('nine')] == 'nine':
+            #print("Qui 'nine'")
+            tradotto +=  '9'
+        #else:
+            #print(f"Qui qualcosa altro --> testo_originale[{posizione}:]={testo_originale[posizione:]}")
+
+    print(f"tradotto={tradotto}")
+    
+    # Ritorna il risultato finale
+    return tradotto
 
 
 def main():
@@ -53,33 +87,22 @@ def main():
         print('-' * 30)
         print(linea.strip())
 
-        restituito_1 = scambia_numero(linea, 'one', '1')
-        print(f"restituito_1:{restituito_1}")
-        estratto_1 = estrai_numero(linea, 'one', '1')
-        print(f"estratto_1:{estratto_1}")
-        restituito_2 = scambia_numero(linea, 'two', '2')
-        print(f"restituito_2:{restituito_2}")
-
-    
-    #risultati=re.findall(r"(one|1))", linea)
-    #for trovato in risultati:
-    #    print(f"Ho trovato:{trovato}")
-
+        tradotto = trasforma_riga(linea.strip())
         prima_cifra = None
         ultima_cifra = None
         numero = None
-        for carattere in linea:
-        #print("[" + carattere + "]")
+        for carattere in tradotto:
+            print("[" + tradotto + "]")
             if sei_un_numero(carattere):
                 if prima_cifra==None:
                     prima_cifra=carattere
                 ultima_cifra=carattere
-        #print("prima_cifra:" + str(prima_cifra))
-        #print("ultima_cifra:" + str(ultima_cifra))
-    #print("Fine linea")
-    #numero = int(prima_cifra + ultima_cifra)
-    #print (numero)
-    #totale += numero
+        print("prima_cifra:" + str(prima_cifra))
+        print("ultima_cifra:" + str(ultima_cifra))
+        print("Fine linea")
+        numero = int(prima_cifra + ultima_cifra)
+        print (numero)
+        totale += numero
     print (totale)
 
 
--- 2023/1/test_elfo.py
+++ 2023/1/test_elfo.py
@@ -2,36 +2,46 @@ import elfo
 
 
 def test_trasforma_riga_singola_cifra():
+    assert elfo.trasforma_riga('1') == '1'
     for num in ['1', '2', '3', '4', '5', '6', '7', '8', '9']:
-        assert num == elfo.trasforma_riga(num)
+        assert elfo.trasforma_riga(num) == num
 
 
-def test_trasforma_riga_singolo_numero_in_lettere():
-    assert '1' == elfo.trasforma_riga("one")
-    assert '2' == elfo.trasforma_riga("two")
-    assert '3' == elfo.trasforma_riga("three")
+def test_trasforma_riga_singola_piu_cifre():
+    assert elfo.trasforma_riga('424242') == '424242'
+
+def test_trasforma_numeri():
+    assert elfo.trasforma_riga("one") == '1'
+    assert elfo.trasforma_riga("two") == '2'
+    assert elfo.trasforma_riga("three") == '3'
     assert '4' == elfo.trasforma_riga("four")
     assert '5' == elfo.trasforma_riga("five")
     assert '6' == elfo.trasforma_riga("six")
     assert '7' == elfo.trasforma_riga("seven")
     assert '8' == elfo.trasforma_riga("eight")
     assert '9' == elfo.trasforma_riga("nine")
-    
 
-
-def test_trasforma_riga_two():
-    assert '2' == elfo.trasforma_riga("two")
-
-
-def test_trasforma_riga_two_one():
-    assert '21' == elfo.trasforma_riga("twoone")
+def test_trasforma_two_one():
+    assert elfo.trasforma_riga("twoone") == '21'
 
 
 def test_trasforma_riga_2_one():
     assert '21' == elfo.trasforma_riga("2one")
 
+def test_trasforma_riga_8_one():
+    assert '81' == elfo.trasforma_riga("8one")
+
+
+def test_trasforma_riga_threeone():
+    assert '31' == elfo.trasforma_riga("threeone")
 
 def test_trasforma_riga_twone():
     assert '21' == elfo.trasforma_riga("twone")
 
+
+def test_trasforma_riga_oneight():
+    assert '18' == elfo.trasforma_riga("oneight")
+
+def test_trasforma_one_two():
+    assert elfo.trasforma_riga("onetwo") == '12'
 
